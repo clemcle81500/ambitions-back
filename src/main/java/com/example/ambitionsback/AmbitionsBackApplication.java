f2f54eda73c1b94d575cf8fcfc7a994793d38fba
clemcle81500 <moulisclement.pro@gmail.com>
2025-10-23 23:25:09 +0200

    Initiailisation du projet

---
+++ src/main/java/com/example/ambitionsback/AmbitionsBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ambitionsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbitionsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbitionsBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ambitionsback/AmbitionsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ambitionsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbitionsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
